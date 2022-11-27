43b3585156a6b12d184ef4b71dd17efc8053de1c
Stanislaw Cruz <stanycruz@outlook.com>
2022-11-27 13:14:06 -0300

    feat: add useRef Example1 - create dom reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import UseRefExample1 from './components/UseRefExample1'
+
 function App() {
   return (
     <div className='container mt-5'>
-      Hello
+      <UseRefExample1 />
     </div>
   )
 }
